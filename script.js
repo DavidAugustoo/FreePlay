771a9157466fe133ddd0b5f04cba3de74fdbab70
David Augusto <davidribeiro086@gmail.com>
2022-02-27 13:29:33 -0300

    :sparkles: feat: animations

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// animation
 
+let target = document.querySelectorAll('[data-anime]');
+
+window.addEventListener('scroll', () => {
+    let windowTop = window.scrollY + ((window.innerHeight * 3) / 4);
+
+    target.forEach(element => {
+        if((windowTop) > element.offsetTop) {
+            element.classList.add('animate');
+        } else {
+            element.classList.remove('animate');
+        }
+    });
+});
